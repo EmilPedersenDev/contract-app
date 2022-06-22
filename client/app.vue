757cfe9c620be2bab99a24b9bdc7754fb9607421
Emil <emil.pedersen@refined.com>
2022-06-22 19:16:46 +0200

    change structure

---
+++ client/app.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
+
+<script setup>
+useHead({
+  title: "Contract App",
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
+  charset: "utf-8",
+  link: {
+    rel: "stylesheet",
+    href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css",
+    integrity:
+      "sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==",
+    crossorigin: "anonymous",
+    referrerpolicy: "no-referrer",
+  },
+});
+</script>
---
+++ client/pages/login.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="login">
+    <div class="login__wrapper">
+      <n-button secondary
+        >Login with Google <i class="fa-brands fa-google"></i>
+      </n-button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { NButton } from "naive-ui";
+</script>
+
+<style lang="scss" scoped>
+.login {
+  height: 100vh;
+  position: relative;
+  .login__wrapper {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    min-height: 300px;
+    width: 100%;
+    max-width: 500px;
+    box-shadow: 0 0 10px 1px rgb(209, 209, 209);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 5px;
+    .n-button {
+      .n-button__content {
+        .fa-brands.fa-google {
+          padding-left: 0.375rem;
+        }
+      }
+    }
+  }
+}
+</style>
